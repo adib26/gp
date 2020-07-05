ff64969201bb01804f30e1715f26d7673bfe04f5
adib <adibwageeh2@gmail.com>
2020-07-06 00:51:44 +0200

    adding register ui

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
-  // login page
   runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Color.fromRGBO(229, 153, 109, 1),
+      backgroundColor: Color.fromRGBO(245, 192, 67, 1),
       appBar: AppBar(
         title: Text(
           'E-learning',
           style: TextStyle(fontSize: 20),
         ),
-        backgroundColor: Color.fromRGBO(100, 35, 39, 1),
+        backgroundColor: Color.fromRGBO(77, 69, 87, 1),
       ),
       body: login(),
     ),
@@ -24,6 +23,7 @@ class login extends StatefulWidget {
   _loginState createState() => _loginState();
 }
 
+// login ui
 class _loginState extends State<login> {
   @override
   Widget build(BuildContext context) {
@@ -66,11 +66,66 @@ class _loginState extends State<login> {
                   child: Text('Login'),
                 ),
                 RaisedButton(
-                  child: Text('Register'),
-                ),
+                    child: Text('Register'),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => register()));
+                    }),
               ],
             ),
           ]),
+    );
+  }
+}
+
+// register ui
+class register extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(245, 192, 67, 1),
+      appBar: AppBar(
+        title: Text(
+          'E-learning',
+          style: TextStyle(fontSize: 20),
+        ),
+        backgroundColor: Color.fromRGBO(77, 69, 87, 1),
+      ),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Register',
+                style: TextStyle(
+                    color: Color.fromRGBO(173, 65, 51, 1),
+                    fontSize: 30,
+                    fontFamily: 'Galada'),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(25, 0, 25, 10),
+                color: Color.fromRGBO(100, 35, 39, 0.3),
+                child: TextField(
+                  decoration: new InputDecoration(
+                      hintText: 'username',
+                      contentPadding: EdgeInsets.fromLTRB(10, 0, 10, 0)),
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(25, 0, 25, 20),
+                color: Color.fromRGBO(100, 35, 39, 0.3),
+                child: TextField(
+                  decoration: new InputDecoration(
+                    hintText: 'password',
+                    contentPadding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  ),
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              RaisedButton(child: Text('Register'), onPressed: () {}),
+            ]),
+      ),
     );
   }
 }
